ea6b3935e588211f60a3e17ec09892fd43e62337
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2015-10-14 11:50:47 -0300

    inicio eventos

---
+++ page-filsa-eventos.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: FILSA 2015 Eventos
+*/
+?>
+<?php get_header(); ?>
+<div id="main-page" class="container_16 cf post-<?php echo $post->ID;?>">
+    
+    <?php get_template_part('parts/clean-sidebar');?>
+
+    
+    <div id="content" class="grid_12">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part('parts/addthis');?>
+            <div class="the-content">
+                <?php get_template_part( 'parts/filsa/eventos-ajax' );?>
+            </div> 
+        <?php endwhile;endif; ?>
+    </div>
+</div>
+</div>
+
+<?php get_footer(); ?>
