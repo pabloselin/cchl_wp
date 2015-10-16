e02271635e7dd54f2df3c8950d0c534f3d2ebadc
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2015-10-16 11:22:17 -0300

    ajuste en addthis

--- parts/addthis.php
+++ parts/addthis.php
@@ -15,11 +15,11 @@
             </div>
  -->
             <div class="sharer" data-url="<?php echo urlencode( get_permalink($post->ID) );?>">
-                  <a target="_blank" href="https://facebook.com/sharer.php?u=<?php echo get_permalink($post->ID);?>" class="sharer__facebook"><i class="fa fa-facebook"></i></a>
+                  <a target="_blank" href="https://facebook.com/sharer.php?u=<?php echo urlencode( get_permalink($post->ID) );?>" class="sharer__facebook"><i class="fa fa-facebook"></i></a>
 
-                  <a target="_blank" href="https://twitter.com/home?status=<?php echo $post->post_title;?> - <?php echo wp_get_shortlink($post->ID);?>" class="sharer__twitter"><i class="fa fa-twitter"></i></a>
+                  <a target="_blank" href="https://twitter.com/home?status=<?php echo urlencode( $post->post_title . ' - '  . wp_get_shortlink($post->ID ) );?>" class="sharer__twitter"><i class="fa fa-twitter"></i></a>
 
-                  <a target="_blank" href="https://plus.google.com/share?url=<?php echo get_permalink($post->ID);?>" class="sharer__gplus"><i class="fa fa-google-plus"></i></a>
+                  <a target="_blank" href="https://plus.google.com/share?url=<?php echo urlencode( get_permalink($post->ID) );?>" class="sharer__gplus"><i class="fa fa-google-plus"></i></a>
 
                   <!-- <a target="_blank" href="whatsapp://send?text=<?php echo $post->post_title;?> <?php echo get_permalink($post->ID);?>" title="Enviar por WhatsApp" class="whatsapp"><i class="fa fa-whatsapp"></i></a> -->
             </div>
