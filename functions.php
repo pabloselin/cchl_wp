50e69501d40fa555ac8d1b3008a611be32ad756f
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2015-10-19 16:35:06 -0300

    accesos rápidos

--- functions.php
+++ functions.php
@@ -8,6 +8,8 @@ define('CCHL_PAGEFILSA', 54798);
 define('CCHL_FILSA2015', 54646);
 define('CCHL_CATSFILSA', '184, 183');
 
+register_nav_menu( 'accesos-rapidos-filsa-2015', 'Accesos Rápidos Filsa 2015' );
+
 include( TEMPLATEPATH . '/inc/oldfunctions.php');
 include( TEMPLATEPATH . '/inc/custom-functions.php');
 include( TEMPLATEPATH . '/inc/scripts.php');
