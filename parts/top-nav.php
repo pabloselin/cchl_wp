ff8e65aec1b40d042ede06614b14b399f8fea683
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2015-12-22 11:56:17 -0300

    menu socios

--- parts/top-nav.php
+++ parts/top-nav.php
@@ -33,8 +33,12 @@
         	<?php wp_nav_menu( array('menu' => 'Principal' )); ?>
         </ul>
         <?php if(!is_home() && (in_category(33)||in_category(32)||in_category(101)||in_category(21)||in_category(68)||is_page(45)||is_page(88)||is_page(144)||is_page(90)) ):?>
-            <ul id="menu-socios-container">
-                <?php wp_nav_menu( array('menu' => 'Socios - interior'));?>
-            </ul>
+                <?php 
+                wp_nav_menu(
+                    array(
+                    'menu' => 'Socios - interior',
+                    'menu_id' => 'menu-socios'
+                        )
+                    );?>
         <?php endif;?>
     </div>
