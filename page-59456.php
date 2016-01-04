99540785a18578b0735520679be4213da3daec3a
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2016-01-04 17:21:21 -0300

    eventos fil viña 2016

--- page-59456.php
+++ page-59456.php
@@ -10,18 +10,7 @@
         </div>
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
           //Llamar contenido Filsa 2014 si corresponde
-          if($post->ID == 108):
-            $filsa2014id = 31817;
-            $filsa2014 = get_page($filsa2014id);
-            ?>
-              <h1><?php echo $filsa2014->post_title; ?></h1>
-           <?php get_template_part('parts/addthis');?>
-            <div class="cf"></div>
-            <?php echo get_the_post_thumbnail($filsa2014->ID, 'imagen_single'); ?>
-            <div class="the-content">
-                <?php echo apply_filters('the_content', $filsa2014->post_content);?>
-            </div>
-          <?php else:
+          
          ?>
             <h1><?php the_title(); ?></h1>
            <?php get_template_part('parts/addthis');?>
@@ -30,9 +19,8 @@
             <div class="the-content">
                 <?php the_content();?>
 
-                <?php get_template_part( 'parts/eventos', 'fil' );?>
+                <?php get_template_part( 'parts/eventos', 'fil-2016' );?>
             </div>
-          <?php endif;?>
 		<?php endwhile;endif; ?>
         
     </div>
