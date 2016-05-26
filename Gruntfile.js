1aee33c905540f1ceb4be4c11c1e87568cd97ffc
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2016-05-26 16:16:29 -0300

    grunt func para cambio de nombres

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,50 @@ module.exports = function(grunt) {
 		watch: {
 			less: {
 				files: ['less/*.less'],
-				tasks: ['less']
+				tasks: ['clean','concat', 'less', 'version']
+			}
+		},
+		version: {
+      assets: {
+        options: {
+          rename: true
+        },
+        files: {
+          'inc/scripts.php': ['css/style-cchl.css', 'js/cchl-scripts.js']
+        }
+      }
+    },
+		clean: {
+      dist: [
+        'css/style-cchl.*.css',
+        'js/cchl-scripts.*.min.js'
+      ]
+    },
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: [
+					'js/src/cchl-ajax.js',
+					'js/src/cchl-main.js',
+					'js/src/cchl-filsa.js'
+				],
+				dest: 'js/cchl-scripts.js'
 			}
 		}
 	});
 	//Load tasks plugins
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-wp-assets');
 	//Default Tasks
-	grunt.registerTask('default', ['less']);
+	grunt.registerTask('default', [
+			'clean',
+			'concat',
+			'less',
+			'version'
+		]);
 }
--- inc/scripts.php
+++ inc/scripts.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 function cchl_styles() {
 	if(!is_admin()) {
-	
+
 	wp_register_style( 'mailchimp', '//cdn-images.mailchimp.com/embedcode/classic-081711.css' , array(), '0.1', 'screen');
 	//gfonts
 	wp_register_style( 'fonts', 'http://fonts.googleapis.com/css?family=Signika:400,300,600,700' , array(), '0.1', 'screen' );
@@ -9,7 +9,7 @@ function cchl_styles() {
 	wp_register_style( 'fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css' , array(), '0.1', 'screen' );
 
 	//Compiled grunt style
-	wp_register_style( 'cchlcss', get_bloginfo('template_url') . '/css/style-cchl.css', array(), '0.2', 'screen' );
+	wp_register_style( 'cchlcss', get_bloginfo('template_url') . '/css/style-cchl.ed3d0ff4.min.css', array(), '0.2', 'screen' );
 
 	wp_enqueue_style('cchlcss');
 	wp_enqueue_style('mailchimp');
@@ -24,9 +24,7 @@ if(!is_admin()) {
 	wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js', array() );
 }
 
-  wp_register_script( 'cchl_main', get_bloginfo('template_url') . '/js/cchl-main.js', array('jquery', 'featherlight'));
-  wp_register_script( 'cchl_ajax', get_bloginfo('template_url') . '/js/cchl-ajax.js', array('jquery', 'masonry'));
-  wp_register_script( 'cchl_filsa', get_bloginfo('template_url') . '/js/cchl-filsa.js', array('jquery', 'cchl_ajax'));
+  wp_register_script( 'cchl_scripts', get_bloginfo('template_url') . '/js/cchl-scripts.f1b4936d.min.js', array('jquery', 'featherlight', 'masonry'));
   wp_register_script( 'cycle', get_bloginfo('template_url') . '/js/jquery.cycle.all.js', array('jquery'));
   wp_register_script( 'readmore', get_bloginfo('template_url') . '/js/readmore.min.js', array('jquery'));
   wp_register_script( 'imagesloaded', get_bloginfo('template_url') . '/js/imagesloaded.min.js', array('jquery'));
@@ -34,10 +32,7 @@ if(!is_admin()) {
   wp_register_script( 'featherlight', get_bloginfo('template_url') . '/js/featherlight/featherlight.min.js', array('jquery'));
   wp_enqueue_script( 'jquery' );
   wp_enqueue_script( 'featherlight' );
-  wp_enqueue_script( 'cchl_events' );
-  wp_enqueue_script( 'cchl_ajax' );
-  wp_enqueue_script( 'cchl_main' );
-  wp_enqueue_script( 'cchl_filsa' );
+  wp_enqueue_script( 'cchl_scripts' );
   wp_enqueue_script( 'cycle' );
   wp_enqueue_script( 'masonry' );
   wp_enqueue_script( 'readmore' );
@@ -58,7 +53,7 @@ if(!is_admin()) {
 
   $tiposarr['ajaxurl'] = admin_url('admin-ajax.php');
 
-  wp_localize_script( 'cchl_ajax', 'cchl', $tiposarr );
+  wp_localize_script( 'cchl_scripts', 'cchl', $tiposarr );
 }
 
 add_action('wp_enqueue_scripts', 'cchl_scripts');
