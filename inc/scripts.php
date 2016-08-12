1a3028ba0e0b4ea7c1a9c0f928fd3db1a392d333
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2016-08-12 09:06:58 -0400

    fix ajustes movil filsa2016

--- inc/scripts.php
+++ inc/scripts.php
@@ -9,7 +9,7 @@ function cchl_styles() {
 	wp_register_style( 'fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css' , array(), '0.1', 'screen' );
 
 	//Compiled grunt style
-	wp_register_style( 'cchlcss', get_bloginfo('template_url') . '/css/style-cchl.c4261cbb.min.css', array(), '0.2', 'screen' );
+	wp_register_style( 'cchlcss', get_bloginfo('template_url') . '/css/style-cchl.826fdfb3.min.css', array(), '0.2', 'screen' );
 
 	wp_enqueue_style('cchlcss');
 	wp_enqueue_style('mailchimp');
