12e30382b4ebf8bf6612964fe852ba5b60d95aec
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2016-08-31 16:13:22 -0300

    boton solapa header special

--- parts/header-special.php
+++ parts/header-special.php
@@ -9,6 +9,9 @@
 
 <div id="header" class="header-special <?php if($socios == true): echo 'header-socios';endif;?>">
 	<img src="<?php bloginfo('template_url');?>/img/filsa2016/header_global_filsa2016.png" alt="Filsa 2016">	
+	<a class="specialplus" href="<?php echo get_permalink(CCHL_FILSA2016);?>" title="Ver más de FILSA 2016">
+		Ver <i class="fa fa-plus"></i>
+	</a>
 	<?php get_template_part( 'parts/mainmenu' );?>
 </div>
 
