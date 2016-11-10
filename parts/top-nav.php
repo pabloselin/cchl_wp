5a9941e3530f330cfdf09ea75dd7750aad8565ff
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2016-11-10 12:23:14 -0300

    cabecera mexico fuera

--- parts/top-nav.php
+++ parts/top-nav.php
@@ -12,7 +12,7 @@
         <h1 id="mainlogo">
           <a href="<?php bloginfo( 'url' ); ?>">Cámara Chilena del Libro 60 años trabajando por el libro y la lectura</a>
       </h1>
-      <a class="header__logofil2016" href="<?php echo get_permalink(CCHL_FILSA2016);?>"><img src="<?php echo get_bloginfo('template_url');?>/img/filsa2016/banner_filsa_2016_animado.gif" alt="<?php echo get_the_title(CCHL_FILSA2016);?>"></a>
+      <!-- <a class="header__logofil2016" href="<?php echo get_permalink(CCHL_FILSA2016);?>"><img src="<?php echo get_bloginfo('template_url');?>/img/filsa2016/banner_filsa_2016_animado.gif" alt="<?php echo get_the_title(CCHL_FILSA2016);?>"></a> -->
 
       <?php get_template_part('parts/mainmenu');?>
 
