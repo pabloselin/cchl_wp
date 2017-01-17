82175c40ce85bd4239673c1ed80dbc7cfc819780
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2017-01-17 18:38:39 -0300

    llamados a menus home contenido

--- home.php
+++ home.php
@@ -2,24 +2,49 @@
 /* Template Name: Inicio */
 ?>
 
+<?php 
+
+  $noticias_home = cchl_getmenus('noticias-home');
+
+?>
+
 <?php get_header(); ?>
 
 <div class="home-responsive container">
   <div class="row">
-    <section class="left-news col-md-8">
-      
-      <div class="row">
-        <article class="article-news-wide col-md-12">
-            
-        </article>
-      </div>
     
-      <div class="row">
-        <article class="article-news-half col-md-6"></article>
-        <article class="article-news-half col-md-6"></article>
-      </div>
     
-    </section>
+
+        <section class="left-news col-md-8">
+
+          <?php if(array_key_exists(0, $noticias_home) ):
+              $noticia_principal = $noticias_home[0];
+          ?>
+
+          <div class="row">
+            <article class="article-news-wide col-md-12">
+
+                <div class="img">
+
+                </div>
+
+                <div class="content">
+                  <h1><?php echo $noticia_principal->title;?></h1>
+                </div>
+
+            </article>
+          </div>
+
+          <?php endif;?>
+        
+          <div class="row">
+            <article class="article-news-half col-md-6"></article>
+            <article class="article-news-half col-md-6"></article>
+          </div>
+        
+        </section>
+
+    
     
     <div class="accesos col-md-4"></div>
   </div>
