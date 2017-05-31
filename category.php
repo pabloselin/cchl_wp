a54972352c62d5caa4651316a1ac5e6b6ab45235
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2017-05-31 19:14:18 -0400

    ajuste en los medios

--- category.php
+++ category.php
@@ -19,6 +19,29 @@
 
 <div class="category-items">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    
+    <?php 
+    $enlosmedios = getGroupOrder('link_link');
+    if($enlosmedios):?>
+    
+    <?php
+        foreach($enlosmedios as $link){ ?> 
+            <article class="col-md-4 item-mini-noticia in-category-item solo-link-item">
+                <a href="<?php echo get('link_link',$link); ?>" target="_blank">
+                <div class="txt">
+                <h2>
+                        <?php the_title();?>
+                </h2>
+                <p><strong>Fuente:</strong> <?php echo get('link_descripcion',$link); ?></p>
+                </div>
+                </a>
+            </article>
+    <?php } ?>   
+
+
+        
+
+    <?php else:?>
         <article class="col-md-4 item-mini-noticia in-category-item">
             <a href="<?php the_permalink();?>">
             <?php if(has_post_thumbnail( )):
@@ -32,6 +55,7 @@
             </div>
             </a>
         </article>
+    <?php endif;?>
     <?php endwhile;?>
     </div>
 </div>
--- inc/scripts.php
+++ inc/scripts.php
@@ -11,9 +11,9 @@ function cchl_styles() {
 	//Compiled grunt style
 	wp_register_style( 'cchlcss', get_bloginfo('template_url') . '/css/style-cchl.885b73f0.min.css', array(), CCHL_VERSION, 'screen' );
 
-  wp_register_style( 'home-cchl', get_bloginfo('template_url') . '/css/home-cchl.5ffa3b9b.min.css', array(), CCHL_VERSION, 'screen' );
+  wp_register_style( 'home-cchl', get_bloginfo('template_url') . '/css/home-cchl.a2997050.min.css', array(), CCHL_VERSION, 'screen' );
 
-  wp_register_style( 'interior-cchl', get_bloginfo('template_url') . '/css/interior-cchl.affa13f0.min.css', array(), CCHL_VERSION, 'screen' );
+  wp_register_style( 'interior-cchl', get_bloginfo('template_url') . '/css/interior-cchl.b47b385c.min.css', array(), CCHL_VERSION, 'screen' );
 
 	if(!is_admin() && !is_home() && !is_single() && !is_category() && get_page_template_slug($post->ID) != 'bs-archivo-ferias.php' && get_page_template_slug($post->ID) != 'bs-default-page.php') {
     
