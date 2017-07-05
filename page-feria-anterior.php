b898ab9f3f32283133e1c5fcb44138095a3ae26f
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2017-07-05 16:48:41 -0400

    extra template check en observatorio integrantes

--- page-feria-anterior.php
+++ page-feria-anterior.php
@@ -29,27 +29,26 @@
                     get_template_part( 'parts/eventos-filvina-2017' );
             endif;?>
 
-             <?php if(is_page_template('page-observatorio-integrantes.php')): ?>
+             <?php if(is_page_template('page-observatorio-integrantes.php') || is_page_template('old/page-observatorio-integrantes.php')): ?>
                  <div class="listado">
-        <ul class="cf">
-            <?php
-            $miembros = getGroupOrder('imagen');
-            foreach($miembros as $miembro){
-				echo "<li>";
-				$otros = array("h" => 95, "w" => 95, "zc" => 1, "q" => 100);
-   				echo get_image('imagen',$miembro,1,1,NULL,$otros);
-				echo "<div class='info'>
-				<h3>".get('nombre',$miembro)."</h3>
-				<span>".get('cargo',$miembro)."</span>";
-                echo '<div class="textoint">'. apply_filters('the_content', get('texto',$miembro)). '</div>
-				</div>
-				</li>';
-			}
-            ?>
-            </ul>
-            </div>
-             <?php endif;?>
-          
+                    <ul class="cf">
+                        <?php
+                        $miembros = getGroupOrder('imagen');
+                        foreach($miembros as $miembro){
+                            echo "<li>";
+                            $otros = array("h" => 95, "w" => 95, "zc" => 1, "q" => 100);
+                            echo get_image('imagen',$miembro,1,1,NULL,$otros);
+                            echo "<div class='info'>
+                            <h3>".get('nombre',$miembro)."</h3>
+                            <span>".get('cargo',$miembro)."</span>";
+                            echo '<div class="textoint">'. apply_filters('the_content', get('texto',$miembro)). '</div>
+                            </div>
+                            </li>';
+                        }
+                        ?>
+                        </ul>
+                </div>
+                <?php endif;?>
 		<?php endwhile;
           endif; ?>
 
