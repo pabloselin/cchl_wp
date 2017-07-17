7fdd822df3e216ef556c1f45f387a0b5e330e513
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2017-07-17 17:10:13 -0400

    404 y menublock

--- 404.php
+++ 404.php
@@ -1,15 +1,38 @@
-<?php
-/* Template Name: Page Interior */
+<?php 
+/*
+Template Name: Página No encontrada
+*/
 ?>
-<?php get_header(); ?>
-<div id="main-page" class="container_16 cf">
-    <div id="content" class="grid_12">
-        <div id="bread">
-        404: No encontrado
-        </div>
-            <h1>No encontrado</h1>
-            
-            <p>Contenido no encontrado.</p> 
+<?php get_header();?>
+
+<div class="container">
+        <?php get_template_part('parts/bs-blocks/bs-breadcrumb');?>
+        <div class="row">        
+                <div class="col-md-8 col-md-offset-2">
+
+                 <article class="page not-found">
+                    <header>
+                        <h1>404: No encontrado</h1>
+                    </header>
+
+                    <div class="article-content text-content">
+                        
+                        <?php do_action('cchl_beforecontent');?>
+
+                        <p>Contenido no encontrado</p>
+                        <p>¿Quizás quieras buscar algún contenido?</p>
+                        <p><?php echo get_search_form(  );?></p>
+                        <p>O puedes <a href="<?php echo get_bloginfo('url');?>">volver a la portada</a></p>
+
+                        <?php do_action('cchl_aftercontent');?>
+                        
+                    </div>
+                </article>
+
+                
+                </div>
+
     </div>
 </div>
-<?php get_footer(); ?>
+
+<?php get_template_part('parts/bs-home/bs-footer'); ?>
--- parts/bs-blocks/bs-menublock.php
+++ parts/bs-blocks/bs-menublock.php
@@ -2,10 +2,11 @@
 	<h2 class="menublock-title col-md-2"><?php echo $menublock_title;?></h2>
     <?php 
         $menublockitems = cchl_getmenus($menu);
-        foreach($menublockitems as $menublockitem) { ?>
+        foreach($menublockitems as $menublockitem) {
+             ?>
 
         <div class="menublock-item col-md-2">
-            <a href="<?php echo get_permalink($menublockitem->object_id);?>">
+            <a href="<?php echo $menublockitem->url;?>">
                 <h3><?php echo $menublockitem->title;?></h3>
             </a>
         </div>
