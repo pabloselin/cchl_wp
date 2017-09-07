0b5bb6670d194f55839ecf01c0dc10b2c50843fc
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2017-09-07 18:36:35 -0300

    title-tag support

--- header-filsa-custom.php
+++ header-filsa-custom.php
@@ -2,7 +2,6 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<title><?php wp_title();?></title>
 <link rel="profile" href="https://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php wp_head();?>
