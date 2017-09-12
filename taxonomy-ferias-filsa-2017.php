e61bf2aead7e35acf8e20e17ce8e6dce9c655017
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2017-09-12 11:17:01 -0300

    tax ferias filsa

---
+++ taxonomy-ferias-filsa-2017.php
@@ -0,0 +1,7 @@
+<?php get_header('filsa-custom');?>
+
+<div class="container">
+    <?php echo filsa2017_run_part('archive-noticias-filsa-2017');?>
+</div>
+
+<?php get_footer('filsa-custom');?>
---
+++ template-filsa-2017-noticias.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Taxonomy Ferias Filsa
+Template Post Type: filsa-2017
+*/
+?>
+
+<?php get_header('filsa-custom');?>
+
+<div class="container">
+    <?php echo filsa2017_run_part('archive-noticias-filsa-2017');?>
+</div>
+
+<?php get_footer('filsa-custom');?>
