5a4b8d4b1ec523d159870095f10f8061077ba6fb
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2017-09-15 16:22:09 -0300

    primer commit visitas guiadas

---
+++ template-visitas-guiadas-colegios.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Visitas Guiadas Colegios
+Template Post Type: filsa-2017
+*/
+?>
+
+<?php get_header('filsa-custom');?>
+
+<div class="container">
+    <?php echo filsa2017_run_part('visitas-guiadas-colegios');?>
+</div>
+
+<?php get_footer('filsa-custom');?>
