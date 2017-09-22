6a73cb3d98b75ec487802a7e9f4c69027b753bea
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2017-09-22 17:14:11 -0300

    template visitas

--- template-visitas-guiadas-colegios.php
+++ template-visitas-guiadas-colegios.php
@@ -1,3 +1,10 @@
+<?php
+/*
+Template Name: Visitas guiadas colegios
+Template Post Type: filsa-2017
+*/
+?>
+
 <?php get_header('filsa-custom');?>
 
 <?php echo filsa2017_run_part('content-filsa-2017', 'visitas-guiadas-colegios');?>
