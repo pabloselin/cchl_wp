744ba6364fb094f9311ff717f59498730febbaf6
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2017-10-04 20:17:55 -0300

    change template invitados

--- template-filsa-2017-invitados.php
+++ template-filsa-2017-invitados.php
@@ -7,7 +7,7 @@ Template Post Type: filsa-2017
 <?php get_header('filsa-custom');?>
 
 <div class="container">
-    <?php echo filsa2017_run_part('content-filsa-2017', 'invitados-filsa-2017');?>
+    <?php echo filsa2017_run_part('content-filsa-2017', 'invitados-filsa-2017-masonry');?>
 </div>
 
 <?php get_footer('filsa-custom'); ?>
