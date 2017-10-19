8dbf7bc591d4631e5a0203975e69791d422cca38
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2017-10-19 15:50:51 -0300

    template filsa colab

---
+++ templates/template-filsa-2017-colaboradores.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+Template Name: Colaboradores
+Template Post Type: filsa-2017
+*/
+?>
+<?php get_header('filsa-custom');?>
+
+<div class="container">
+    <?php echo filsa2017_run_part('content-filsa-2017', 'colaboradores');?>
+</div>
+
+<?php get_footer('filsa-custom'); ?>
