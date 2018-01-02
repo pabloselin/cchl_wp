a4038ec43e027c6add6c1c4b49c38bf1c2e605d6
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2018-01-02 12:14:44 -0300

    ajuste css faltante y plantilla expositores

---
+++ parts/bs-blocks/bs-expositores.php
@@ -0,0 +1,30 @@
+<div class="expositores-feria-std">
+<?php 
+  $args = array(
+    'post_type' => 'tribe_organizer',
+    'numberposts' => -1,
+    'orderby' => 'post_title',
+    'order' => 'ASC',
+    'tax_query' => array(
+      array(
+        'taxonomy' => 'ferias',
+        'field' => 'slug',
+        'terms' => 'fil-vina-2018'
+      )
+    )
+  );
+  $expositores = get_posts($args);
+  foreach($expositores as $expositor):
+  $location = get_post_meta($expositor->ID, 'cchl_location', true);
+  ?>
+
+    <div class="expositor-item">
+        <h4><?php echo $expositor->post_title;?></h4>
+        <?php if($location):?>
+        <p class="ubicacion"><span class="label label-info">Stand <?php echo $location;?></span></p>
+        <?php endif;?>
+    </div>
+<?php
+    endforeach;
+?>
+</div>
