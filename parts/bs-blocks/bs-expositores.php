20cbd8ecc8b268a4132f308cb12563bcf50f365b
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2018-01-02 12:29:49 -0300

    ajuste altura y columnas expositores filvina

--- parts/bs-blocks/bs-expositores.php
+++ parts/bs-blocks/bs-expositores.php
@@ -1,4 +1,4 @@
-<div class="expositores-feria-std">
+<div class="expositores-feria-std row">
 <?php 
   $args = array(
     'post_type' => 'tribe_organizer',
@@ -17,13 +17,14 @@
   foreach($expositores as $expositor):
   $location = get_post_meta($expositor->ID, 'cchl_location', true);
   ?>
-
+<div class="col-md-6">
     <div class="expositor-item">
         <h4><?php echo $expositor->post_title;?></h4>
         <?php if($location):?>
         <p class="ubicacion"><span class="label label-info">Stand <?php echo $location;?></span></p>
         <?php endif;?>
     </div>
+</div>
 <?php
     endforeach;
 ?>
